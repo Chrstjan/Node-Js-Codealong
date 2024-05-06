2c7221c26d1a58a95184f013207053607142d5a1
Chrstjan <ps5christian@hotmail.com>
2024-05-06 13:44:46 +0200

    Started making server

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import http from 'http';
 
+const port = 4000;
+
 http.createServer((req, res) => {
-    console.log("Server is running on http://localhost:4000");
+    console.log(`Webserver is now running on http://localhost:${port}`);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello world');
+    res.end();
 }).listen(4000)
 
-console.log('Server initialized on http://localhost:4000');
+console.log(`Server is initialized on http://localhost:${port}`);
