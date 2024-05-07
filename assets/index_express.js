9a9822667d86511ae360dc0398a6d30610c9ebdf
Christian <ps5christian@hotmail.com>
2024-05-07 13:41:18 +0200

    Made the code for endpoints that doesnt exist

--- assets/index_express.js
+++ assets/index_express.js
@@ -1,41 +1,49 @@
-import express from 'express';
+import express from "express";
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-    console.log(req.query);
-    res.send('Forside');
-})
+app.get("/", (req, res) => {
+  console.log(req.query);
+  res.send("Forside");
+});
 
-app.post('/', (req, res) => {
-    res.send('Endpoint til post');
-})
+app.post("/", (req, res) => {
+  res.send("Endpoint til post");
+});
 
-app.get('/about', (req, res) => {
-    res.send('Hvem er vi');
-})
+app.get("/about", (req, res) => {
+  res.send("Hvem er vi");
+});
 
-app.get('/work', (req, res) => {
-    res.send('Hvad kan vi');
-})
+app.get("/work", (req, res) => {
+  res.send("Hvad kan vi");
+});
 
-app.get('/find-us', (req, res) => {
-    res.send('Find os');
-})
+app.get("/find-us", (req, res) => {
+  res.send("Find os");
+});
 
-app.get('/products', (req, res) => {
-    res.send('Produkt liste');
-})
+app.get("/products", (req, res) => {
+  res.send("Produkt liste");
+});
 
-app.get('/productDetails', (req, res) => {
-    res.send('Produkt detailjer');
-})
+app.get("/productDetails", (req, res) => {
+  res.send("Produkt detailjer");
+});
 
-app.get('/contact', (req, res) => {
-    res.send('Kontakt side');
-})
+app.get("/categories", (req, res) => {
+  res.send("kategorie liste");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Kontakt side");
+});
+
+app.use((req, res) => {
+  res.status(404).send("Siden blev ikke fundet");
+});
 
 app.listen(port, () => {
-    console.log(`Webserver is running on http://localhost:${port}`);
-})
+  console.log(`Webserver is running on http://localhost:${port}`);
+});
