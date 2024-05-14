a4823ac855e86e5f5d0ebd363a2d8a1740c4f49c
Chrstjan <ps5christian@hotmail.com>
2024-05-14 13:52:00 +0200

    Made the first controller code

--- assets/index_express.js
+++ assets/index_express.js
@@ -1,9 +1,6 @@
 import express from "express";
 import dotenv from 'dotenv';
-
-import SongModel from "./Models/song.model.js";
-import albumModel from "./Models/album.model.js";
-import artistModel from "./Models/artist.model.js";
+import { SongController } from "./Controllers/song.controller.js";
 
 const app = express();
 
@@ -11,66 +8,11 @@ dotenv.config();
 
 const port = process.env.PORT;
 
-console.log(process.env.API_KEY);
+app.use(SongController);
 
-app.get("/", (req, res) => {
-  console.log(req.query);
-  res.send("Forside");
-});
-
-app.get('/songs', async (req, res) => {
-  const data = await SongModel.getAllRecords()
-  console.log(data);
-  res.send(data);
-});
-
-app.get('/artists', async (req, res) => {
-  const data = await artistModel.getAllArtists()
-  console.log(data);
-  res.send(data);
-})
-
-app.get('/albums', async (req, res) => {
- const data = await albumModel.getAllAlbums()
- console.log(data);
- res.send(data);
-});
-
-app.post("/", (req, res) => {
-  res.send("Endpoint til post");
-});
-
-app.get("/about", (req, res) => {
-  res.send("Hvem er vi");
-});
-
-app.get("/work", (req, res) => {
-  res.send("Hvad kan vi");
-});
-
-app.get("/find-us", (req, res) => {
-  res.send("Find os");
-});
-
-app.get("/products", (req, res) => {
-  res.send("Produkt liste");
-});
-
-app.get("/productDetails", (req, res) => {
-  res.send("Produkt detailjer");
-});
-
-app.get("/categories", (req, res) => {
-  res.send("kategorie liste");
-});
-
-app.get("/contact", (req, res) => {
-  res.send("Kontakt side");
-});
-
-app.use((req, res) => {
-  res.status(404).send("Siden blev ikke fundet");
-});
+// app.use((req, res) => {
+//   res.status(404).send("Siden blev ikke fundet");
+// });
 
 app.listen(port, () => {
   console.log(`Webserver is running on http://localhost:${port}`);
